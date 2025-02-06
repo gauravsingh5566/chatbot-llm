52370aa3a8c1df7153356a7be4d831c2c07735c3
Ubuntu <ubuntu@ip-172-31-2-231.ap-south-1.compute.internal>
2025-02-06 13:10:58 +0000

    new

--- backend/start.sh
+++ backend/start.sh
@@ -2,4 +2,4 @@
 source /home/ubuntu/chatbot-llm/backend/llama-chatbot-env/bin/activate  # Activate your virtual environment
 export FLASK_APP=app.py  # Replace with your actual Flask app name if different
 export FLASK_ENV=production
-gunicorn -w 4 -b 0.0.0.0:5000 app:app 
+gunicorn -w 1 -b 0.0.0.0:5000 app:app 
